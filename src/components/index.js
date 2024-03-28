b2ca61d3ed05df9991527818d28e23028733f73a
Joseph <jotienoochieng@gmail.com>
2024-03-28 15:01:22 +0300

    created contact component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import CarWidget from "./CartWidget/CarWidget";
 import Products from "./Products/Products";
 import Quantifier from "./Quantifier/Quantifier";
 import Cart from "./Cart/Cart";
+import Contact from "./contact/Contact";
 
-export { Header, CarWidget, Products, Quantifier, Cart };
+export { Header, CarWidget, Products, Quantifier, Cart, Contact };
